81da864ed15b344af6957fcd5820682cf39a2385
Ilia Kuznetsov <ilyakuz@amazon.com>
2023-05-05 00:14:44 +0100

    feat: Use redcon to parse AOF replication

--- pkg/resp/resp.go
+++ pkg/resp/resp.go
@@ -2,28 +2,27 @@ package resp
 
 import (
 	"github.com/kuzznya/go-redis-search-replica/pkg/exec"
-	"github.com/kuzznya/go-redis-search-replica/pkg/resp/proto"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
+	"github.com/tidwall/redcon"
 	"io"
-	"reflect"
 	"strconv"
 	"strings"
 )
 
 func NewParser(rd io.Reader) *Parser {
 	c := &countingReader{r: rd}
-	return &Parser{rd: proto.NewReader(c), c: c}
+	return &Parser{rd: redcon.NewReader(c), c: c}
 }
 
 type Parser struct {
-	rd *proto.Reader
+	rd *redcon.Reader
 	c  *countingReader
 }
 
 // ParseCmd returns the data, count of bytes read and error (if any)
 func (p *Parser) ParseCmd() (exec.Command, uint64, error) {
-	data, err := p.rd.ReadReply(SliceParser)
+	data, err := p.rd.ReadCommand()
 	offset := p.c.offset
 	p.c.offset = 0
 
@@ -31,15 +30,9 @@ func (p *Parser) ParseCmd() (exec.Command, uint64, error) {
 		return nil, offset, err
 	}
 
-	parts, ok := data.([]any)
-	if !ok {
-		return nil, offset, nil // TODO check if nil as err is ok
-	}
+	parts := data.Args
 
-	name, ok := parts[0].(string)
-	if !ok {
-		return nil, offset, nil
-	}
+	name := string(parts[0])
 
 	name = strings.ToUpper(name)
 	switch name {
@@ -76,94 +69,56 @@ func (p *Parser) ParseCmd() (exec.Command, uint64, error) {
 	return nil, offset, nil
 }
 
-func parseSet(args []any) (exec.Command, error) {
+func parseSet(args [][]byte) (exec.Command, error) {
 	if len(args) == 0 {
 		log.Warnln("Not enough args for SET, skipping")
 		return nil, nil
 	}
 
-	key, ok := args[0].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse SET: unknown key type %s", reflect.TypeOf(args[0]).Name())
-	}
-
+	key := string(args[0])
 	return exec.SetCmd{Key: key}, nil
 }
 
-func parseHset(args []any) (exec.Command, error) {
+func parseHset(args [][]byte) (exec.Command, error) {
 	if len(args) < 2 {
 		log.Warnln("Not enough args for HSET, skipping")
 		return nil, nil
 	}
 
-	key, ok := args[0].(string)
-	if !ok {
-		return nil, errors.New("Failed to parse HSET: unknown key type")
-	}
+	key := string(args[0])
 
 	hsetArgs := make([]exec.HSetArg, (len(args)-1)/2)
 	for i := 1; i < len(args)-1; i += 2 {
-		field, ok := args[i].(string)
-		if !ok {
-			return nil, errors.New("Failed to parse HSET")
-		}
+		field := string(args[i])
 		hsetArgs[(i-1)/2].Field = field
-
-		strValue, ok := args[i+1].(string)
-		if !ok {
-			return nil, errors.Errorf("Failed to parse HSET: unknown value type %s", reflect.TypeOf(args[i+1]).Name())
-		}
-
-		hsetArgs[(i-1)/2].Value = []byte(strValue)
+		hsetArgs[(i-1)/2].Value = args[i+1]
 	}
 
 	return exec.HSetCmd{Key: key, Args: hsetArgs}, nil
 }
 
-func parseHsetnx(args []any) (exec.Command, error) {
+func parseHsetnx(args [][]byte) (exec.Command, error) {
 	if len(args) < 3 {
 		log.Warnln("Not enough args for HSETNX, skipping")
 		return nil, nil
 	}
 
-	key, ok := args[0].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse HINCRBY: unknown key type %s", reflect.TypeOf(args[0]).Name())
-	}
+	key := string(args[0])
+	field := string(args[1])
+	value := args[2]
 
-	field, ok := args[1].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse HINCRBY: unknown field name type %s", reflect.TypeOf(args[1]).Name())
-	}
-
-	value, ok := args[2].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse HSETNX: unknown value type %s", reflect.TypeOf(args[2]).Name())
-	}
-
-	return exec.HsetnxCmd{Key: key, Field: field, Value: []byte(value)}, nil
+	return exec.HsetnxCmd{Key: key, Field: field, Value: value}, nil
 }
 
-func parseHincrby(args []any) (exec.Command, error) {
+func parseHincrby(args [][]byte) (exec.Command, error) {
 	if len(args) < 3 {
 		log.Warnln("Not enough args for HINCRBY, skipping")
 		return nil, nil
 	}
 
-	key, ok := args[0].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse HINCRBY: unknown key type %s", reflect.TypeOf(args[0]).Name())
-	}
-
-	field, ok := args[1].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse HINCRBY: unknown field name type %s", reflect.TypeOf(args[1]).Name())
-	}
-
-	incrStr, ok := args[2].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse HINCRBY: unknown increment type %s", reflect.TypeOf(args[2]).Name())
-	}
+	key := string(args[0])
+	field := string(args[1])
+	incrStr := string(args[2])
 
 	incr, err := strconv.ParseInt(incrStr, 10, 64)
 	if err != nil {
@@ -173,28 +128,22 @@ func parseHincrby(args []any) (exec.Command, error) {
 	return exec.HincrbyCmd{Key: key, Field: field, Value: incr}, nil
 }
 
-func parseHdel(args []any) (exec.Command, error) {
+func parseHdel(args [][]byte) (exec.Command, error) {
 	if len(args) < 2 {
 		log.Warnln("Not enough args for HDEL, skipping")
 		return nil, nil
 	}
 
-	key, ok := args[0].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse HDEL: unknown key type %s", reflect.TypeOf(args[0]).Name())
-	}
+	key := string(args[0])
 
 	fields := make([]string, len(args)-1)
 	for i, field := range args[1:] {
-		fields[i], ok = field.(string)
-		if !ok {
-			return nil, errors.Errorf("Failed to parse HDEL: unknown field name type %s", reflect.TypeOf(field).Name())
-		}
+		fields[i] = string(field)
 	}
 	return exec.HDelCmd{Key: key, Fields: fields}, nil
 }
 
-func parseDel(args []any) (exec.Command, error) {
+func parseDel(args [][]byte) (exec.Command, error) {
 	if len(args) == 0 {
 		log.Warnln("Not enough args for DEL, skipping")
 		return nil, nil
@@ -202,58 +151,27 @@ func parseDel(args []any) (exec.Command, error) {
 
 	keys := make([]string, len(args))
 	for i, arg := range args {
-		key, ok := arg.(string)
-		if !ok {
-			return nil, errors.Errorf("Failed to parse DEL: unknown key type %s", reflect.TypeOf(args[0]).Name())
-		}
+		key := string(arg)
 		keys[i] = key
 	}
 
 	return exec.DelCmd{Keys: keys}, nil
 }
 
-func parseRename(args []any, nx bool) (exec.Command, error) {
+func parseRename(args [][]byte, nx bool) (exec.Command, error) {
 	if len(args) < 2 {
 		log.Warnln("Not enough args for RENAME/RENAMENX, skipping")
 		return nil, nil
 	}
 
-	key, ok := args[0].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse RENAME/RENAMENX: unknown key type %s", reflect.TypeOf(args[0]).Name())
-	}
-
-	newKey, ok := args[1].(string)
-	if !ok {
-		return nil, errors.Errorf("Failed to parse RENAME/RENAMENX: unknown key type %s", reflect.TypeOf(args[0]).Name())
-	}
+	key := string(args[0])
+	newKey := string(args[1])
 
 	if nx {
 		return exec.RenamenxCmd{Key: key, NewKey: newKey}, nil
 	} else {
 		return exec.RenameCmd{Key: key, NewKey: newKey}, nil
 	}
-}
-
-// sliceParser implements proto.MultiBulkParse.
-func SliceParser(rd *proto.Reader, n int64) (interface{}, error) {
-	vals := make([]interface{}, n)
-	for i := 0; i < len(vals); i++ {
-		v, err := rd.ReadReply(SliceParser)
-		if err != nil {
-			if err == proto.Nil {
-				vals[i] = nil
-				continue
-			}
-			if err, ok := err.(proto.RedisError); ok {
-				vals[i] = err
-				continue
-			}
-			return nil, err
-		}
-		vals[i] = v
-	}
-	return vals, nil
 }
 
 type countingReader struct {
